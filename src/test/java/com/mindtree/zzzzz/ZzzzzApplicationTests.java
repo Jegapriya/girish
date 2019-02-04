61c59929b56f54b57519f74f2391834c69f98c4f
Jegapriya <45451985+Jegapriya@users.noreply.github.com>
2019-02-04 15:58:07 +0530

    Update ZzzzzApplicationTests.java

--- src/test/java/com/mindtree/zzzzz/ZzzzzApplicationTests.java
+++ src/test/java/com/mindtree/zzzzz/ZzzzzApplicationTests.java
@@ -1,6 +1,6 @@
 package com.mindtree.zzzzz;
 
-import org.junit.Test;
+/*import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -15,3 +15,4 @@ public class ZzzzzApplicationTests {
 
 }
 
+*/
